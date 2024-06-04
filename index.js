e5dd164530d0b3f04cb3f27671712f08fc3664a0
Subir Mondal <sprivacc0@gmail.com>
2024-06-04 13:34:37 +0530

    code upload

---
+++ index.js
@@ -0,0 +1,38 @@
+// Sidebar
+let open_menu = document.getElementById('open_menu');
+let close_menu = document.getElementById('close_menu');
+let sidebar = document.getElementById('sidebar');
+
+open_menu.addEventListener('click', () => {
+    sidebar.classList.add('open');
+})
+
+close_menu.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+})
+
+
+// Dropdown Function
+let d_item = document.querySelectorAll('.sidebar_d_items');
+d_item.forEach((item) => {
+    item.addEventListener('click', () =>{
+        let temp_class = document.querySelector(`#${item.id.toString().concat('_list')}`);
+        temp_class.classList.toggle('active');
+    })
+})
+
+
+// Login
+let login_model = document.getElementById('login_model');
+let login_o_btn = document.getElementById('login_btn');
+let login_c_btn = document.getElementById('close_l_model');
+
+login_o_btn.addEventListener('click', () => {
+    login_model.classList.add('active');
+})
+
+login_c_btn.addEventListener('click', () => {
+    login_model.classList.remove('active');
+})
+
+
